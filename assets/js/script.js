2482dc56b9b008bbbdb99af481a95a0ca5faf0fd
Tommy McG <thomasjmcgovern98@gmail.com>
2022-03-20 18:34:08 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,17 +58,28 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     //console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     //console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     //console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     //console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
-  update: function(event) {
+  drop: function(event) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
+    update: function(event) {
     var tempArr = [];
 
     $(this).children().each(function () {
@@ -222,7 +233,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -256,6 +267,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 }
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  })
+}, (60 * 1000) * 30);
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
